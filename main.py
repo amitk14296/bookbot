41d72522b388d34aae169a04c435516d4962b5d6
Amit <amitk14296@gmail.com>
2025-03-18 08:57:36 +0530

    Addigng sys

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
-from stats import get_num_words
+import sys
+#from stats import get_num_words
 from stats import count_char
 from stats import report
 from stats import sort_on
-book = "books/frankenstein.txt"
+
+
 #This function will take file path and return contents of the file as string
 def get_book_text(path_to_file):
     #open the file hold that file in f
@@ -17,21 +19,16 @@ def get_book_text(path_to_file):
 
 def main():
 
-    book = "books/frankenstein.txt"
+
     unsorted_list=[]
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)#exit with error code if incorrect number of argument are passed
 
-        #Printing number of words in a book
-    num_words = get_num_words(get_book_text(book))
-    print(f"Found {num_words} total words")
-
-        #Calculating occurence of each character in the book
-    character = count_char(book)
-    print(character)
-
-
+    filepath = sys.argv[1]
+    unsorted_list = report(count_char(filepath))
 
         #List of dictionary
-    unsorted_list = report(character)
     unsorted_list.sort(key=sort_on, reverse=True)
     for items in unsorted_list:
         for key, value in items.items():
